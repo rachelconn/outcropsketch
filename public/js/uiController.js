68de5c1b780ea2341179235e8630f473c8c1342c
bwilliford <bwilliford@gmail.com>
2020-08-04 14:05:50 -0500

    Basic stuff set up

--- public/js/uiController.js
+++ public/js/uiController.js
@@ -13,26 +13,6 @@ function toggleMenu() {
     }
 }
 
-function changeTool(tool) {
-    document.getElementById('sketch').className = "";
-    document.getElementById('erase').className = "";
-    if (tool === "sketch") {
-        if (step === 2 || step === 3) {
-            axis.activate();
-        }
-        else {
-            outline.activate();
-        }
-        
-        document.getElementById('sketch').className = "active";
-    }
-    else {
-        eraser.activate();
-        document.getElementById('erase').className = "active";
-    }
-}
-
-
 function toggleRegister() {
     if (registerActive) {
         registerActive = 0;
@@ -42,45 +22,4 @@ function toggleRegister() {
         registerActive = 1;
         document.getElementById('registerOverlay').className = 'active';
     }
-}
-
-function chooseShape(shape) {
-    document.getElementById('choicePanel_bean').className = 'choiceOption';
-    document.getElementById('choicePanel_oval').className = 'choiceOption';
-    document.getElementById('choicePanel_subTriangular').className = 'choiceOption';
-    document.getElementById('choicePanel_subRectangular').className = 'choiceOption';
-
-    document.getElementById(shape).className = 'choiceOption active';
-
-    setTimeout(function() { nextStep()}, 1000);
-    
-}
-
-function chooseSize(size) {
-    document.getElementById('small').className = 'choiceOption';
-    document.getElementById('medium').className = 'choiceOption';
-    document.getElementById('large').className = 'choiceOption';
-
-    document.getElementById(size).className = 'choiceOption active';
-
-    setTimeout(function() { nextStep()}, 1000);
-}
-
-function chooseOrnamentation(ornamentation) {
-    if (document.getElementById(ornamentation).className === 'choiceOption') {
-        document.getElementById(ornamentation).className = 'choiceOption active';
-    }
-    else {
-        document.getElementById(ornamentation).className = 'choiceOption';
-    }
-}
-
-function changeFilter(type) {
-    document.getElementById('default').className = '';
-    document.getElementById('edges').className = '';
-    document.getElementById('posterized').className = '';
-
-    document.getElementById(type).className = 'active';
-    document.getElementById('sketchCanvas').style.background = "#000000 url('images/geo-"+type+".jpg') no-repeat";
-    document.getElementById('sketchCanvas').style.backgroundSize = "100vw";
 }
