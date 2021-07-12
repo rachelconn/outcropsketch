32c00780058098554111999bb0806e6ec24a0cd8
rconn478 <rconn478@tamu.edu>
2021-07-12 12:03:50 -0500

    Fix fill eraser behavior on CompoundPath (#6)

--- src/tools/eraser.ts
+++ src/tools/eraser.ts
@@ -20,6 +20,30 @@ export default function createEraserTool(props: EraserProps = {}): paper.Tool {
   function erase(point: paper.Point) {
     let didErase = false;
 
+    const eraseCompoundPathChild = (item: paper.Item) => {
+      // CompoundPaths have multiple children, if the removed item is one of these,
+      // then remove other children that represent holes/exclusions in the CompoundPath
+      const itemClockwise = (item as paper.PathItem).clockwise;
+      const compoundPathItems = item.parent.children as paper.PathItem[];
+
+      // Must iterate backwards since item.remove() results in item being removed from children
+      for (let i = compoundPathItems.length - 1; i >= 0; i--) {
+        const child = compoundPathItems[i];
+
+        // Opposite winding direction indicates that child should be a hole within the path
+        if (child === item || child.clockwise === itemClockwise) continue;
+
+        // Don't remove if child is not contained within the item,
+        // since it represents a hole within a different part of the path
+        if (child !== item && item.bounds.contains(child.bounds)) {
+          child.remove();
+        }
+      }
+
+      item.remove();
+      didErase = true;
+    };
+
     // Delete all items within range of the cursor
     paper.project.hitTestAll(point, hitTestOptions).forEach(({ item, type }) => {
       // Don't erase label text, must erase the item it's labeling instead
@@ -28,29 +52,18 @@ export default function createEraserTool(props: EraserProps = {}): paper.Tool {
       // If an item isn't filled and its fill is hit rather than the stroke, don't erase
       if (type === 'fill' && item.fillColor === undefined) return;
 
-      // CompoundPaths have multiple children, if the removed item is one of these,
-      // then remove other children that represent holes/exclusions in the CompoundPath
-      if (item.parent instanceof paper.CompoundPath) {
-        const itemClockwise = (item as paper.PathItem).clockwise;
-        const compoundPathItems = item.parent.children as paper.PathItem[];
-
-        // Must iterate backwards since item.remove() results in item being removed from children
-        for (let i = compoundPathItems.length - 1; i >= 0; i--) {
-          const child = compoundPathItems[i];
-
-          // Opposite winding direction indicates that child should be a hole within the path
-          if (child === item || child.clockwise === itemClockwise) continue;
-
-          // Don't remove if child is not contained within the item,
-          // since it represents a hole within a different part of the path
-          if (child !== item && item.bounds.contains(child.bounds)) {
-            child.remove();
-          }
+      // Handle compound paths
+      if (item instanceof paper.CompoundPath) {
+        for (let i = item.children.length - 1; i >= 0; i--) {
+          const child = item.children[i];
+          if (child?.hitTest(point, hitTestOptions)) eraseCompoundPathChild(child);
         }
       }
-
-      item.remove();
-      didErase = true;
+      else if (item.parent instanceof paper.CompoundPath) eraseCompoundPathChild(item);
+      else {
+        item.remove();
+        didErase = true;
+      }
     });
 
     // Add state to undo history (if any items were erased)
