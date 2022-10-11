fb823be396c9a0344bb86146890dec7f0c311cee
Rachel Conn <rconn478@gmail.com>
2022-10-10 21:52:50 -0500

    Create Checkbox component

---
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import styles from '../InputField/InputField.css';
+
+interface CheckboxProps {
+  children: string,
+  name: string,
+  onChange: (value: boolean) => any,
+};
+
+const Checkbox: React.FC<CheckboxProps> = ({ children, name, onChange }) => {
+  return (
+    <div className={styles.inputContainer}>
+      <label htmlFor={name}>{`${children}:`}</label>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        name={name}
+        onChange={(e) => onChange(e.currentTarget.checked)}
+      />
+    </div>
+  );
+};
+
+export default Checkbox;
