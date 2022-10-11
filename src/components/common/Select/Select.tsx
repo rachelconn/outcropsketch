20e5455e9cd45b0f89db3777b5fb361da9299aae
Rachel Conn <rconn478@gmail.com>
2022-10-10 21:52:55 -0500

    Create Select component

---
+++ src/components/common/Select/Select.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import styles from '../InputField/InputField.css';
+
+interface SelectProps {
+  options: string[],
+  name: string,
+  children: string,
+  onChange: (value: string) => any,
+};
+
+const Select: React.FC<SelectProps> = ({
+  options,
+  name,
+  children,
+  onChange,
+}) => {
+  const optionComponents = ['', ...options].map((option) => {
+    const handleClick = () => onChange(option);
+
+    return (
+      <option
+        className={styles.option}
+        value={option}
+        onClick={handleClick}
+        key={option}
+      >
+        {option || '—'}
+      </option>
+    );
+  });
+
+  return (
+    <div className={styles.inputContainer}>
+      <label htmlFor={name}>{`${children}: `}</label>
+      <select className={`${styles.input} ${styles.select}`} name={name}>
+        {optionComponents}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
