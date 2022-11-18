815893eaf43b1aad2445d76f4b286a4972928297
Rachel Conn <rconn478@gmail.com>
2022-11-18 12:50:17 -0600

    Create user manager to simplify creating users

--- webserver/authentication/models.py
+++ webserver/authentication/models.py
@@ -1,6 +1,27 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 from django.utils import timezone
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, *_, email, first_name, last_name, password, student, instructor, researcher, **extra_fields):
+        # Process input
+        email = self.normalize_email(email)
+
+        # Save user model
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            student=student,
+            instructor=instructor,
+            researcher=researcher,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser):
     """ Custom user class containing more data """
@@ -12,7 +33,10 @@ class User(AbstractBaseUser):
 
     # Setting overrides
     USERNAME_FIELD = 'email'
+    objects = UserManager()
 
     # Custom fields
-    instructor = models.BooleanField(default=False)
+    instructor = models.BooleanField()
+    researcher = models.BooleanField()
+    student = models.BooleanField()
     admin = models.BooleanField(default=False)
