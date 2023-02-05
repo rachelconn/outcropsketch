29b98bd51d4407c55d53e38b3ec687922381e710
Rachel Conn <rconn478@gmail.com>
2023-02-05 12:16:48 -0600

    Create Course model

---
+++ webserver/courses/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=300, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
