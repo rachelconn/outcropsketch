ad4b7632495ebc5c054fc6a3b4038be582a1ef5c
Rachel Conn <rconn478@gmail.com>
2023-02-07 11:15:01 -0600

    Create get_roles API endpoint

--- webserver/authentication/urls.py
+++ webserver/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from authentication.views import register, login, logout
+from authentication.views import register, login, logout, get_roles
 
 urlpatterns = [
     path('register', register),
     path('login', login),
     path('logout', logout),
+    path('get_roles', get_roles),
 ]
