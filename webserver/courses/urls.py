8781c71b838b331731f5a3f2e181ad8a4a1c0d2c
Rachel Conn <rconn478@gmail.com>
2023-02-08 14:02:33 -0600

    Create join course API endpoint

--- webserver/courses/urls.py
+++ webserver/courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from courses.views import create_course
+from courses.views import create_course, join_course
 
 urlpatterns = [
     path('create', create_course),
+    path('join', join_course),
 ]
