dc91e2ecb7e341c77ecc8fdcccfc64565ac3379a
Rachel Conn <rconn478@gmail.com>
2023-02-08 14:02:42 -0600

    Create UI for joining courses

--- src/components/CoursesPage/MyCoursesPage.tsx
+++ src/components/CoursesPage/MyCoursesPage.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import Cookies from 'js-cookie';
 import { RouteComponentProps } from '@reach/router';
+import checkIcon from '../../icons/check.svg';
 import Article from '../common/Article';
 import styles from'./styles.css';
 import Button from '../common/Button/Button';
 import addIcon from '../../icons/add.svg';
 import pencilIcon from '../../icons/pencil.svg';
+import Dialog from '../common/Dialog/Dialog';
+import InputField from '../common/InputField/InputField';
 
 interface Roles {
   instructor: boolean,
@@ -14,6 +18,8 @@ interface Roles {
 
 const MyCoursesPage: React.FC<RouteComponentProps> = ({ navigate }) => {
   const [roles, setRoles] = React.useState<Roles>();
+  const [courseCode, setCourseCode] = React.useState('');
+  const [useCourseCodeDialogVisible, setUseCourseCodeDialogVisible] = React.useState(false);
 
   React.useEffect(() => {
     fetch('/auth/get_roles').then((response) => response.json()).then((responseJSON) => {
@@ -22,9 +28,32 @@ const MyCoursesPage: React.FC<RouteComponentProps> = ({ navigate }) => {
   }, []);
 
   const handleCreateCourseClick = () => navigate('create');
-  const handleUseCourseCodeClick = () => navigate('use_code');
+  const handleUseCourseCodeClick = () => {
+    setCourseCode('');
+    setUseCourseCodeDialogVisible(true);
+  }
+  const handleCourseCodeChange = (value: string) => setCourseCode(value);
   const createCourseButton = roles?.instructor ? <Button icon={addIcon} onClick={handleCreateCourseClick}>Create Course</Button> : undefined;
   const useCourseCodeButton = roles?.student ? <Button icon={pencilIcon} onClick={handleUseCourseCodeClick}>Use Course Code</Button> : undefined;
+
+  const handleSubmitCodeButtonClick = () => {
+    const body = JSON.stringify({ id: courseCode })
+    fetch('/courses/join', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': Cookies.get('csrftoken'),
+      },
+      body,
+    }).then((response) => {
+      if (!response.ok) throw new Error(`An error occcurred trying to create a new course: ${response}`)
+      // TODO: refresh course list to show added course
+      setUseCourseCodeDialogVisible(false);
+    }).catch((e) => {
+      console.log(e);
+      // TODO: show modal with error message
+    });
+  };
 
   // TODO: show list of your courses here once the API is complete
   return (
@@ -38,6 +67,15 @@ const MyCoursesPage: React.FC<RouteComponentProps> = ({ navigate }) => {
           {useCourseCodeButton}
         </div>
       </div>
+      <Dialog visible={useCourseCodeDialogVisible} onClickOutside={() => setUseCourseCodeDialogVisible(false)}>
+        <div className={styles.dialogContent}>
+          <Article.Header>Enter Course Code</Article.Header>
+          <InputField name="coursecode" onChange={handleCourseCodeChange} >Course code</InputField>
+          <div className={styles.submitCodeButtonContainer}>
+            <Button disabled={courseCode.length < 6} icon={checkIcon} onClick={handleSubmitCodeButtonClick}>Submit</Button>
+          </div>
+        </div>
+      </Dialog>
     </>
   );
 };
