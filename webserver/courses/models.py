cde25e74eff93d184323fb8990470222257b0a15
Rachel Conn <rconn478@gmail.com>
2023-02-08 14:02:17 -0600

    Add students field to Course model

--- webserver/courses/models.py
+++ webserver/courses/models.py
@@ -6,3 +6,4 @@ class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=300, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    students = models.ManyToManyField(User, related_name='joined_courses')
