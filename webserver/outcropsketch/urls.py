fb0dbaa41f11930f1e930ceb941e9a2b3694d897
jungin <jungin@tamu.edu>
2023-02-09 19:00:03 -0600

    link to postgres and outcrop-image-storage in Google cloud

--- webserver/outcropsketch/urls.py
+++ webserver/outcropsketch/urls.py
@@ -6,6 +6,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.conf import settings
 from django.urls import include, path, re_path
 from django.views import static
+from django.conf.urls.static import static as static2
 
 urlpatterns = [
     # TODO: not sure if static paths are still resolved when debug = False, add a check before addingif they are
@@ -16,4 +17,7 @@ urlpatterns = [
     path('uploads/', include('uploads.urls')),
     path('courses/', include('courses.urls')),
     path('', include('frontend.urls')),
-]
+]+ static2(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+        urlpatterns += static2(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
