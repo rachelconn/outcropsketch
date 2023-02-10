6c13833dbc78529547e75b4bdfb0e2c66d065f4b
jungin <jungin@tamu.edu>
2023-02-09 19:00:03 -0600

    link to postgres and outcrop-image-storage in Google cloud

--- webserver/uploads/urls.py
+++ webserver/uploads/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from uploads.views import upload
+# from uploads.views import image_request
+
+# app_name = 'uploadapp'
+# urlpatterns = [
+#     path('image_upload', image_request, name="image_upload")
+# ]
 
 urlpatterns = [
     path('upload', upload)
--- webserver/uploads/views.py
+++ webserver/uploads/views.py
@@ -1,45 +1,32 @@
-from rest_framework.decorators import api_view, parser_classes
-from rest_framework.parsers import MultiPartParser
-from rest_framework.response import Response
-from rest_framework import status
-from django.db import IntegrityError
 from django.shortcuts import render
-from rest_framework import serializers
-from .models import FileUpload
-
-# from django.db import IntegrityError
-# from django.contrib.auth import get_user_model, authenticate, login as django_login, logout as django_logout
-
-# Create your views here.
-
-class FileUploadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FileUpload
-        fields = ('file', )
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.response import Response
+from .models import File
 
 @api_view(['POST'])
-@parser_classes([MultiPartParser])
 def upload(request):
-    if request.method == 'POST':
-        file_serializer = FileUploadSerializer(data=request.data)
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    img_params = {}
+    required_field = 'image'
+    if required_field not in request.data:
+        return Response(
+            data={
+                'reason': f"Required field {required_field.replace('_', ' ')} missing from request.",
+            },
+            status=400,
+        )
+    img_params[required_field] = request.data[required_field]
+    img_params['name'] = request.data[required_field].name
 
-# from django.shortcuts import render
-# from django.conf import settings
-# from django.core.files.storage import FileSystemStorage
-
-# def simple_upload(request):
-#     if request.method == 'POST' and request.FILES['myfile']:
-#         myfile = request.FILES['myfile']
-#         fs = FileSystemStorage()
-#         filename = fs.save(myfile.name, myfile)
-#         uploaded_file_url = fs.url(filename)
-#         return render(request, 'core/simple_upload.html', {
-#             'uploaded_file_url': uploaded_file_url
-#         })
-#     return render(request, 'core/simple_upload.html')
+    try:
+        img = File.objects.upload_file(
+            **img_params,
+        )
+        return Response()
+    except Exception as e:
+        return Response(
+            data={
+                'reason': "Unable to upload the file. Try again later.",
+            },
+            status=400,
+        )
 
