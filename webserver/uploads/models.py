c11000abb7d2ddc8cb39cab4cefa0a7b870b1dfd
jungin <jungin@tamu.edu>
2023-02-09 19:00:03 -0600

    link to postgres and outcrop-image-storage in Google cloud

--- webserver/uploads/models.py
+++ webserver/uploads/models.py
@@ -1,24 +1,38 @@
+from storages.backends.gcloud import GoogleCloudStorage
 from django.db import models
-from authentication.models import User
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 # Create your models here.
+storage = GoogleCloudStorage()
+User = get_user_model()
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return f'labels_{instance.user.id}_%Y%m%d%H%M%S%f'
 
-class FileUpload(models.Model):
-    name = models.CharField(max_length=100)
+class FileManager(models.Manager):
+    use_in_migrations = True
+
+    def upload_file(self, *_, image, name, **extra_fields):
+        img = self.model(
+            image = image,
+            name = name,
+            **extra_fields,
+        )
+        img.save(using=self._db)
+        return img
+class File(models.Model):
+    name = models.CharField(max_length=100, default="image_name")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=user_directory_path)
- 
+    image = models.ImageField(upload_to='images/')  
+    
+    objects = FileManager()
+  
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = ['name']
-
-
-
+        db_table = "upload"
+        ordering = ['-created_at']
