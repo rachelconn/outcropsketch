06b18802de60b3a60f55a6b56a968ab1ca43b097
jungin <jungin@tamu.edu>
2023-02-13 11:14:06 -0600

    changing a model name from file to labeled image

--- webserver/uploads/models.py
+++ webserver/uploads/models.py
@@ -11,14 +11,12 @@ def user_directory_path(*_):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return f'labeled/{uuid.uuid4()}'
 
-class File(models.Model):
+class LabeledImage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=100, default="image_name")
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_images')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='labeled_images')
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.FileField(upload_to=user_directory_path)
-    # image = models.ImageField(upload_to='outcrop-image-storage')
-    # updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
