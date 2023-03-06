855a4c42c0838a7404c294ab8edd0f4c34f38391
jungin <jungin@tamu.edu>
2023-03-05 18:47:13 -0600

    remove commented lines

--- webserver/uploads/urls.py
+++ webserver/uploads/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
 from uploads.views import upload
-# from uploads.views import image_request
-
-# app_name = 'uploadapp'
-# urlpatterns = [
-#     path('image_upload', image_request, name="image_upload")
-# ]
 
 urlpatterns = [
     path('upload', upload)
