757cfa945ad9baed45119a452f010830f5453f3a
jungin <jungin@tamu.edu>
2023-03-05 18:48:33 -0600

    Fix minor issues in uploading images

--- webserver/uploads/views.py
+++ webserver/uploads/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
-from .models import File
-import uuid
+from .models import LabeledImage
+
 
 @api_view(['POST'])
 def upload(request):
@@ -20,7 +20,7 @@ def upload(request):
             },
             status=400,
         )
-    img = File.objects.create(
+    img = LabeledImage.objects.create(
         image = request.data['image'],
         name = request.data['image'].name,
         owner = request.user,
