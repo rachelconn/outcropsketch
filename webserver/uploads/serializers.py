c34aa250e54f8e4a81935788425d397141f19560
jungin <jungin@tamu.edu>
2023-03-05 18:49:11 -0600

    Add routes for adding/listing images in a course

---
+++ webserver/uploads/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import LabeledImage
+
+class LabeledImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LabeledImage
+        fields = ('id', 'name', 'owner', 'created_at', 'image')
