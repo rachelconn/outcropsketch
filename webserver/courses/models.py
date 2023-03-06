bae7c5cdc8f9fed58434e0dadb30d96610148312
jungin <jungin@tamu.edu>
2023-03-05 18:49:11 -0600

    Add routes for adding/listing images in a course

--- webserver/courses/models.py
+++ webserver/courses/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from authentication.models import User
+from uploads.models import LabeledImage
+
+from storages.backends.gcloud import GoogleCloudStorage
+storage = GoogleCloudStorage()
 
 # Create your models here.
 class Course(models.Model):
@@ -7,3 +11,12 @@ class Course(models.Model):
     description = models.CharField(max_length=300, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_courses')
     students = models.ManyToManyField(User, related_name='joined_courses')
+    images = models.ManyToManyField(LabeledImage, blank=True)
+
+
+    def delete(self, *args, **kwargs):
+        # Remove all linked image files when deleting Course object
+        for image in self.images.all():
+            storage.delete(image.image.name)
+            image.delete()
+        super().delete(*args, **kwargs)
