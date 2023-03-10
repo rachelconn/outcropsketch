31d94dccceee54436ba5709ab2dd4c1a0f93e4a4
Rachel Conn <rconn478@gmail.com>
2023-03-10 11:56:43 -0600

    Use standardized ErrorResponse class for 400 responses

--- webserver/uploads/views.py
+++ webserver/uploads/views.py
@@ -1,30 +1,20 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
-from .models import LabeledImage
+
+from common.response import ErrorResponse
+from uploads.models import LabeledImage
 
 
 @api_view(['POST'])
 def upload(request):
     if request.user.is_anonymous:
-        return Response(
-            data=dict(
-                reason='You must be logged in to create a course.',
-            ),
-            status=400,
-        )
+        return ErrorResponse('You must be logged in to create a course.')
     if not request.data['image']:
-        return Response(
-            data={
-                'reason: an image is missing from request.',
-            },
-            status=400,
-        )
+        return ErrorResponse('An image is missing from request.')
     img = LabeledImage.objects.create(
         image = request.data['image'],
         name = request.data['image'].name,
         owner = request.user,
     )
-    return Response(
-    )
-    
+    return Response()
