3ec08b2cc8c0370361f22d34cd1965a3d3fb8444
Rachel Conn <rconn478@gmail.com>
2023-03-17 13:50:58 -0500

    Create delete image view

--- webserver/courses/urls.py
+++ webserver/courses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from courses.views import (
     add_image_to_course,
     create_course,
+    delete_image,
     get_course_info,
     list_courses,
     join_course,
@@ -13,4 +14,5 @@ urlpatterns = [
     path('list', list_courses),
     path('join', join_course),
     path('add_image/<int:id>', add_image_to_course),
+    path('delete_image/<slug:id>', delete_image),
 ]
