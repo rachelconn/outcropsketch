b71f0a5ed12c75debf85d1ba1cd6c620a49f8b28
Rachel Conn <rconn478@gmail.com>
2023-03-21 11:50:18 -0500

    Create StudentAnnotation model

--- webserver/uploads/models.py
+++ webserver/uploads/models.py
@@ -14,6 +14,9 @@ def json_file_path(*_):
 def thumbnail_path(*_):
     return f'labeled/thumbs/{uuid.uuid4()}.jpg'
 
+def annotation_path(*_):
+    return f'labeled/annotations/{uuid.uuid4()}'
+
 class LabeledImage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=100, default="image_name")
@@ -29,3 +32,16 @@ class LabeledImage(models.Model):
     class Meta:
         db_table = 'labeled_image'
         ordering = ['-created_at']
+
+class StudentAnnotation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='annotations')
+    labeled_image = models.ForeignKey(LabeledImage, on_delete=models.CASCADE, related_name='annotations')
+    annotation = models.FileField(upload_to=annotation_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'student_annotation'
+        constraints = [
+            models.UniqueConstraint(fields=['owner', 'labeled_image'], name='annotation by user'),
+        ]
