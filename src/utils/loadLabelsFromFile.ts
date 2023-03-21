b0de21303b805c0fc58aa513174104cd80621851
Rachel Conn <rconn478@gmail.com>
2023-03-21 11:53:16 -0500

    Load student annotation on opening a labeled image

--- src/utils/loadLabelsFromFile.ts
+++ src/utils/loadLabelsFromFile.ts
@@ -6,46 +6,47 @@ import { waitForProjectLoad } from '../redux/reducers/undoHistory';
 import store from '../redux/store';
 import { versionLoadable } from './exportProjectToJSON';
 
-/**
- * Loads labels from a .json file containing a serialized paper.js project
- * @param s String containing a serialized paper.js project
- */
-export function loadLabelsFromString(s: string, loadIfBlank = true, propagateError = true): Promise<void> {
+export function loadLabelsFromJSON(json: SerializedProject, loadIfBlank = true, propagateError = true): Promise<void> {
   // Serialize the current project state in case something goes wrong
   const currentState = paper.project.exportJSON();
 
   return waitForProjectLoad().then(() => {
-    const { image, imageName, project, version } = JSON.parse(s) as SerializedProject;
+    const { image, imageName, project, version } = json;
 
     // Make sure data in the file has the expected properties, otherwise it cannot be handled
-    if (!image || !project || !imageName) {
+    if (!image || project === undefined || !imageName) {
       throw new Error('Label file in unexpected format.');
     }
 
     if (!versionLoadable(version)) throw new Error('Attempted to load incompatible label version.');
 
-    // Remember layer opacities so they can be restored when loading
-    const layerOpacities = new Map<string, number>(
-      paper.project.layers.map((layer) => [layer.name, layer.opacity])
-    );
+    // Don't load project if completely blank (meaning a remote image with no annotation data)
+    if (project) {
+      // Remember layer opacities so they can be restored when loading
+      const layerOpacities = new Map<string, number>(
+        paper.project.layers.map((layer) => [layer.name, layer.opacity])
+      );
 
-    // Clear existing labels: not clearing the project completely beforehand
-    // makes layers incorrectly deserialize
-    paper.project.clear();
+      // // Clear existing labels: not clearing the project completely beforehand
+      // // makes layers incorrectly deserialize
+      paper.project.clear();
 
-    // Load new labels from file
-    paper.project.importJSON(project);
+      // Load new labels from file
+      paper.project.importJSON(project);
 
-    // Restore previously set layer visibilities
-    layerOpacities.forEach((opacity, layerName) => {
-      let layer = paper.project.layers[layerName];
-      if (!layer) layer = new paper.Layer({ name: layerName });
-      layer.opacity = opacity;
-    });
+      // Restore previously set layer visibilities
+      layerOpacities.forEach((opacity, layerName) => {
+        let layer = paper.project.layers[layerName];
+        if (!layer) {
+          layer = new paper.Layer({ name: layerName });
+        }
+        layer.opacity = opacity;
+      });
 
-    if (!loadIfBlank) {
-      // If loadIfBlank isn't set, check if the loaded project is blank and reset to old state if so
-      if (!paper.project.layers.some((layer) => layer.children.length)) throw new Error('Project was blank.');
+      if (!loadIfBlank) {
+        // If loadIfBlank isn't set, check if the loaded project is blank and reset to old state if so
+        if (!paper.project.layers.some((layer) => layer.children.length)) throw new Error('Project was blank.');
+      }
     }
 
     // Load image from file
@@ -56,6 +57,14 @@ export function loadLabelsFromString(s: string, loadIfBlank = true, propagateErr
     paper.project.importJSON(currentState);
     if (propagateError) throw e;
   });
+}
+
+/**
+ * Loads labels from a .json file containing a serialized paper.js project
+ * @param s String containing a serialized paper.js project
+ */
+export function loadLabelsFromString(s: string, loadIfBlank = true, propagateError = true): Promise<void> {
+  return loadLabelsFromJSON(JSON.parse(s) as SerializedProject, loadIfBlank, propagateError);
 }
 
 /**
